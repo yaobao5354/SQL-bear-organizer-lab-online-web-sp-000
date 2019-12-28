094f9c3b385fda34cc03a0c69bfc43d0c723d09b
yaoyu bao <yaobao5354@gmail.com>
2019-12-28 04:21:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,6 @@
 
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender == 'F';"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -8,7 +8,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive == '1' ORDER BY age;"
+  "SELECT name, age FROM bears WHERE alive = '1' ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -20,13 +20,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears WHERE color == 'dark brown';"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears WHERE temperament == 'goofy'"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT name FROM bears WHERE name == null;"
+  "SELECT * FROM bears WHERE name = null;"
 end
